5fc702b2babc6edfcb02954617f0804ffb694779
Ado148 <agreal148@gmail.com>
2025-07-19 13:05:20 +0200

    Initial commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+</head>
+<body>
+    <h1>Posts Index page</h1>
+    <div>My name is {{ $username }} and my age is {{ $age }}</div>
+
+    
+</body>
+</html>
+
+
+
