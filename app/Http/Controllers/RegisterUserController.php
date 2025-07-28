231120b4e0c561f96134de92cbcb9999068b19a9
Ado148 <agreal148@gmail.com>
2025-07-28 12:04:03 +0200

    Added register, login, logout

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterUserController extends Controller
 {
-    //
+    public function register()
+    {
+        return view('auth.register'); // auth is a dir
+    }
+
+    public function store(Request $request)
+    {
+        // validate request 
+        $request->validate([
+            'name' => ['required', 'max:255', 'min:5', 'string'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8', 'confirmed', Password::defaults()]
+        ]);
+
+        // create new user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // log in the user
+        auth()->login($user);
+
+        // redirect the user
+        return to_route('posts.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginUserController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -10,3 +12,8 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome'); //just return view
 
 Route::resource('/posts', PostController::class);
+Route::get('/register', [RegisterUserController::class, 'register'])->name('register'); //definition of function for register
+Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
+Route::get('/login', [LoginUserController::class, 'login'])->name('login');
+Route::post('/login', [LoginUserController::class, 'store'])->name('login.store');
+Route::post('/logout', [LoginUserController::class, 'logout'])->name('logout');
